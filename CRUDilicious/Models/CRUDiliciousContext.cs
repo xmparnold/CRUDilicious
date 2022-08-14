325624b3c9448b7098699610f7bea22b5a8118a1
Matt Arnold <matt.arnd@gmail.com>
2022-08-14 18:10:28 -0500

    initial commit of assingment

---
+++ CRUDilicious/Models/CRUDiliciousContext.cs
@@ -0,0 +1,17 @@
+#pragma warning disable CS8618
+/* 
+Disabled Warning: "Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable."
+We can disable this safely because we know the framework will assign non-null values when it constructs this class for us.
+*/
+using Microsoft.EntityFrameworkCore;
+namespace CRUDilicious.Models;
+// the MyContext class representing a session with our MySQL database, allowing us to query for or save data
+public class CRUDiliciousContext : DbContext
+{
+
+    public CRUDiliciousContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Dish> Dishes { get; set; }
+    
+
+}
